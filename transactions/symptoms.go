f0e9c3dbda90cfdbda3c8a22325849fa72054577
halfwit <michaelmisch1985@gmail.com>
2019-12-04 11:01:33 -0800

    Add note about intentions for symptoms.SendEmail

---
+++ transactions/symptoms.go
@@ -0,0 +1,7 @@
+package transactions
+
+// SendEmail - Run on hook from completed payment
+func SendEmail(id string) {
+	// TODO(halfwit) after receipt of payment on the temp wallet, Doctor receives invoice and client receives symptoms link
+	// This is not done through messages at all!
+}
